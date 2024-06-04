1703f6f2c11d392456ac6d3250ca79b2876ad5ec
Coachonko <info@coachonko.com>
2024-06-04 11:34:51 +0200

    Minor improvements

--- packages/utils/src/nisha.js
+++ packages/utils/src/nisha.js
@@ -1,4 +1,3 @@
 // nisha is a function that works similarly to a ternary operator.
-// The main difference between nisha and a ternary operator is that values are delayed in execution 
-// until they are returned by nisha.
+// Because nisha is a function, parameters are evaluated immediately, not conditionally.
 export const nisha = (condition, truthyValue, falsyValue) => condition ? truthyValue : falsyValue
--- packages/utils/src/nishaho.js
+++ packages/utils/src/nishaho.js
@@ -1,12 +1,11 @@
-// nishaho is a function that works exactly like an if/else-if/else statement
+// nishaho is a function that works exactly like an if/else-if/else statement, or two chained ternary operators.
+// Just like nisha, all parameters are evaluated immediately, not conditionally.
 // If conditionOne is met, returnOne is returned, otherwise conditionTwo is evaluated and if it is met, returnTwo is returned.
 // If none of the conditions are met, the defaultReturn is returned.
-export const nishaho = (defaultReturn, conditionOne, returnOne, conditionTwo, returnTwo) => {
-  if (conditionOne) {
-    return returnOne
-  }
-  if (conditionTwo) {
-    return returnTwo
-  }
-  return defaultReturn
-}
+export const nishaho = (
+  defaultReturn,
+  conditionOne,
+  returnOne,
+  conditionTwo,
+  returnTwo
+) => conditionOne ? returnOne : conditionTwo ? returnTwo : defaultReturn
