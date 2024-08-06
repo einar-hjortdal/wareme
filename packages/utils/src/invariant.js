7b3cea563a42dd6a996e1b194d4307e3e8a64e22
Coachonko <info@coachonko.com>
2024-08-06 16:48:52 +0200

    Link to bun define

--- packages/utils/src/invariant.js
+++ packages/utils/src/invariant.js
@@ -2,12 +2,15 @@ import { illegal } from '@dark-engine/core'
 
 const throwError = (errorMsg) => illegal(errorMsg, 'invariant')
 
+// invariant is meant to be used with Bun's `define`
+// https://bun.sh/guides/runtime/define-constant
 export function invariant (condition, errorMsg) {
   if (condition) {
     return
   }
 
-  // Bun evaluates this condition at bundle time and removes code below it
+  // Bun evaluates this condition at bundle time and removes code below it,
+  // resulting in smaller production bundles.
   if (process.env.BUN_ENV === 'production') {
     throwError('fail')
   }
