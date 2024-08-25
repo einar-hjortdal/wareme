579a1524ae5509e62a7df6b8a91fdb775f427d65
Coachonko <info@coachonko.com>
2024-08-25 22:05:42 +0200

    Fix broken hashKey util

--- packages/utils/src/data.js
+++ packages/utils/src/data.js
@@ -4,13 +4,10 @@ import { detectIsPlainObject } from "./detect"
 export const hashKey = (queryKey) => {
   return JSON.stringify(queryKey, (_key, val) => {
     if (detectIsPlainObject(val)) {
-      const sortedKeys = keys(val).sort()
-      const result = []
-      for (let i = 0, len = sortedKeys.length; i < len; i++) {
-        const key = sortedKeys[i]
-        result.push({ [key]: val[key] })
-      }
-      return result
+      keys(val).sort().reduce((result, key) => {
+        result[key] = val[key]
+        return result
+      }, {})
     }
     return val
   })
