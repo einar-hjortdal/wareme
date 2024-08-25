e5059f8fec259fef7c7e4c20ce3b0b7304938759
Coachonko <info@coachonko.com>
2024-08-25 08:35:38 +0200

    Add detectIsEmptyString

--- packages/utils/src/detect.js
+++ packages/utils/src/detect.js
@@ -2,4 +2,5 @@ import { keys } from '@dark-engine/core'
 
 export const detectIsEmptyArray = (a) => a.length === 0
 export const detectIsEmptyObject = (o) => keys(o).length === 0
+export const detectIsEmptyString = (s) => s === ''
 export const detectIsNaN = (o) => Number.isNaN(o)
